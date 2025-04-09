e3723271fcced5887f582c694e71af4b189045b8
Andrew Wason <rectalogic@rectalogic.com>
2025-04-09 16:43:56 -0400

    use a type guard

--- src/jetstream.ts
+++ src/jetstream.ts
@@ -22,6 +22,25 @@ export interface BlueskyPost {
 export type BlueskyPostHandler = ((event: BlueskyPost) => void) | null;
 export type ErrorHandler = ((event: Event) => void) | null;
 
+function isCommitCreateEvent(event: unknown): event is CommitCreateEvent {
+  if (
+    typeof event === "object" &&
+    event !== null &&
+    "kind" in event &&
+    event.kind === "commit" &&
+    "commit" in event &&
+    typeof event.commit === "object" &&
+    event.commit !== null &&
+    "operation" in event.commit &&
+    event.commit.operation === "create" &&
+    "record" in event.commit
+  ) {
+    return AppBskyFeedPost.isRecord(event.commit?.record);
+  } else {
+    return false;
+  }
+}
+
 export default class Jetstream {
   #ws: WebSocket | null = null;
   #onmessage: BlueskyPostHandler = null;
@@ -48,24 +67,16 @@ export default class Jetstream {
   private handleMessage(event: MessageEvent<string>) {
     if (this.#onmessage) {
       const data = JSON.parse(event.data);
-      const record: unknown = data?.commit?.record;
       if (
-        data?.kind === "commit" && data?.commit?.operation === "create"
+        isCommitCreateEvent(data) &&
+        data.commit.record.text.length >= 20 &&
+        data.commit.record.langs?.includes("en")
       ) {
-        if (AppBskyFeedPost.isRecord(record)) {
-          const event: CommitCreateEvent = data;
-          if (
-            event.commit.record.text.length >= 20 &&
-            event.commit.record.langs &&
-            event.commit.record.langs.includes("en")
-          ) {
-            this.#onmessage({
-              uri: `at://${event.did}/app.bsky.feed.post/${event.commit.rkey}`,
-              cid: event.commit.cid,
-              text: event.commit.record.text,
-            });
-          }
-        }
+        this.#onmessage({
+          uri: `at://${data.did}/app.bsky.feed.post/${data.commit.rkey}`,
+          cid: data.commit.cid,
+          text: data.commit.record.text,
+        });
       }
     }
   }
